1545cd4343e1a7836995a7dde992080fe45c196c
Dominique Climenti <dominique.climenti@kyos.ch>
2023-12-24 23:55:52 +0100

    starting to integrate web searches

--- geppetto.js
+++ geppetto.js
@@ -1,3 +1,6 @@
+// define the search url
+searchUrl = "https://searx.thele.me/?q=";
+
 // hos and key for the API
 const OPENAI_API_KEY = "sk-Skynet-openchatKEY";
 const OPENAI_API_ENDPOINT = "https://chatapi.thele.me/v1/chat/completions";
@@ -27,6 +30,7 @@ async function sendChatMessage(message) {
     );
     const messageBody = listMessageBody.item(listMessageBody.length - 1);
     messageBody.innerHTML = "";
+    console.log("system insert 2");
     history.push({
       role: "system",
       content:
@@ -36,6 +40,38 @@ async function sendChatMessage(message) {
     });
     browser.storage.local.set({ hist: JSON.stringify(history) });
     enableChat();
+    //
+    // answer with internet command
+    //
+  } else if (message.endsWith("+i")) {
+    text = message.replace("+i", "");
+    history.push({
+      role: "user",
+      content: text,
+    });
+    let searchQuery = await getSearchQuery(history).then((response) => {
+      return response;
+    });
+    console.log("searchQuery: " + searchQuery);
+    searchQuery = searchQuery.replace('"', "");
+    console.log(history);
+    const listMessageBody = document.querySelectorAll(
+      ".chatgeppetto-message-body"
+    );
+    const messageBody = listMessageBody.item(listMessageBody.length - 1);
+    console.log("Searching the web for: " + searchQuery);
+    urlsearch = searchUrl + encodeURIComponent(searchQuery);
+    console.log("urlsearch: " + urlsearch);
+    let searchResults = await getSearchResults(urlsearch);
+    console.log("searchResults: " + searchResults);
+    history.push({
+      role: "user",
+      content: text,
+    });
+    getResponse(history).then((response) => {
+      enableChat();
+    });
+    return;
     //
     // Read page command
     //
@@ -48,6 +84,7 @@ async function sendChatMessage(message) {
     );
     const messageBody = listMessageBody.item(listMessageBody.length - 1);
     messageBody.innerHTML = "";
+    console.log("system insert 3");
     history.push({
       role: "system",
       content:
@@ -111,11 +148,15 @@ async function sendChatMessage(message) {
     );
     enableChat();
     return;
+  } else {
+    //
+    // Normal message
+    //
+    await getResponse(history).then((response) => {
+      enableChat();
+    });
   }
-  //
-  // Normal message
-  //
-  getResponse(history);
+  return;
 }
 
 async function getResponse(history) {
@@ -126,7 +167,7 @@ async function getResponse(history) {
     },
     payload: JSON.stringify({
       messages: history,
-      mode: "chat-instruct",
+      mode: "chat",
       instruction_template: "Mistral",
       character: "ChatGeppetto",
       stream: true,
@@ -274,4 +315,122 @@ function readPageContent() {
   text = text.trim();
   command = "/readpagecontent " + text;
   sendChatMessage(command);
+}
+
+async function getSearchQuery(history) {
+  var localhistory = history;
+  localhistory.push({
+    role: "user",
+    content:
+      "Ne réponds pas à la question, donne juste des mots clés à chercher sur Google pour trouver la réponse. soit facuel, ajoute aucun text. Donne juste une liste de mots clés.",
+  });
+  const searchQuery = await fetch(OPENAI_API_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      messages: localhistory,
+      mode: "instruct",
+      instruction_template: "Mistral",
+      stream: false,
+      temperature: 1,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data: ");
+      console.log(data);
+      return data.choices[0].message.content;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  return searchQuery;
+}
+
+async function getWebSearchResult(url) {
+  text = "";
+  await fetch(url, { origin: "https://searx.thele.me" })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((html) => {
+      var doc = new DOMParser().parseFromString(html, "text/html");
+      elementList = doc.querySelectorAll([
+        "p",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "figcaption",
+      ]);
+      for (u = 0; u < elementList.length; u++) {
+        text = text + "\n\n" + elementList.item(u).textContent.trim();
+      }
+      text = text.trim();
+    })
+    .catch((error) => {
+      console.error(error);
+      sendBtn.disabled = false;
+      sendInput.disabled = false;
+      sendInput.focus();
+    });
+  return text;
+}
+
+async function getSearchResults(url) {
+  let text = "";
+  let urllist = [];
+  pagecontent = await fetch(url, { origin: "https://searx.thele.me" }).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP ${response.status}`);
+      }
+      return response.text();
+    }
+  );
+  var doc = new DOMParser().parseFromString(pagecontent, "text/html");
+  let pagelist = "Here are a bunch of results to help you answer:\n\n";
+  elementList = doc.querySelectorAll(["a"]);
+  for (u = 0; u < elementList.length - 1; u++) {
+    if (
+      elementList.item(u).href.includes("proxy.thele.me") ||
+      elementList.item(u).href.includes("searx.thele.me") ||
+      elementList.item(u).href.includes("web.archive.org")
+    )
+      continue;
+    url = elementList.item(u).href.trim();
+    console.log("URL: " + url);
+    urllist.push(url);
+  }
+  urllist = urllist.filter(onlyUnique);
+  urllist = urllist.slice(0, 5);
+  let urlnum = urllist.length;
+  for (t = 0; t < urllist.length; t++) {
+    const listMessageBody = document.querySelectorAll(
+      ".chatgeppetto-message-body"
+    );
+    const messageBody = listMessageBody.item(listMessageBody.length - 1);
+    messageBody.innerHTML = "Reading page " + (t + 1) + "/" + urllist.length;
+    pagelist =
+      pagelist +
+      urllist[t] +
+      "\n\n-----\n\n" +
+      (await getWebSearchResult(urllist[t])) +
+      "\n\n----------\n\n";
+  }
+  history.pop();
+  pagelist =
+    pagelist +
+    "Read and remember them carefully, you will be tested on them later.";
+  console.log("system insert 1");
+  history.push({ role: "system", content: pagelist });
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+  return pagelist;
 }
