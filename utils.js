085a69867644b6f14d1dd57dcf05ff773dda7eae
Dominique Climenti <dominique.climenti@kyos.ch>
2023-12-24 15:22:44 +0100

    Refactoring: Moved utilitary function to utils.js to improve code readability

---
+++ utils.js
@@ -0,0 +1,88 @@
+function enableChat() {
+  sendBtn.disabled = false;
+  sendInput.disabled = false;
+  sendInput.focus();
+}
+
+function disableChat() {
+  sendBtn.disabled = true;
+  sendInput.disabled = true;
+}
+
+//
+// Function to turn the widget on or off
+// @return void
+//
+function toggleGeppetto() {
+  chatVisible = !chatVisible;
+  chatWidget.classList.toggle("visible", chatVisible);
+  chatToggle.innerText = chatVisible ? "Close" : "Chat";
+  chatInput.focus();
+  show = !show;
+  browser.storage.local.set({ visible: show });
+}
+
+//
+// Callback functions to read history from the local storage
+// @param item: the item to read
+// @return void
+//
+function onGotHist(item) {
+  history = JSON.parse(item.hist);
+  var name = "";
+  for (var i = 0; i < history.length; i++) {
+    if (history[i].role == "user") {
+      name = "You";
+    } else if (history[i].role == "assistant") {
+      name = "ChatGeppetto";
+    } else {
+      continue;
+    }
+    var converter = new showdown.Converter();
+    converter.setFlavor("github");
+    var html = converter.makeHtml(history[i].content);
+    addChatMessage(name, html);
+    hljs.highlightAll();
+  }
+}
+
+//
+// Callback functions to handle errors when reading history from he local storage
+// @param error: the error to handle
+// @return void
+//
+function onErrorHist(error) {
+  history = [];
+}
+
+//
+// Callback functions to read the visibility of the widget from the local storage
+// @param item: the item to read
+// @return void
+//
+function onGotShow(item) {
+  chatVisible = item.visible;
+  chatWidget.classList.toggle("visible", chatVisible);
+  chatToggle.innerText = chatVisible ? "Close" : "Chat";
+  chatInput.focus();
+  show = item.visible;
+}
+
+//
+// Callback functions to handle errors when reading the visibility of the widget from the local storage
+// @param error: the error to handle
+// @return void
+//
+function onErrorShow(error) {
+  show = false;
+}
+
+//
+// Function to handle keypress events
+// @param e: the event to handle
+// @return void
+//
+function KeyPress(e) {
+  var evtobj = window.event ? event : e;
+  if (evtobj.keyCode == 89 && evtobj.ctrlKey) toggleGeppetto();
+}
