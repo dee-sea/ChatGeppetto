8f62160313de66dc1275753e3a7f8b99e88dca7e
Dominique Climenti <dominique.climenti@kyos.ch>
2023-12-25 15:20:08 +0100

    Feature: Added statement.js to handle all texts

---
+++ statements.js
@@ -0,0 +1,18 @@
+statements = {
+  readText: {
+    fr: "Lis attentivement et souviens toi du texte suivant:",
+    en: "Read carefully and remember the following text:",
+  },
+  longSeparator: {
+    fr: "\n\n----------\n\n",
+    en: "\n\n----------\n\n",
+  },
+  shortSeparator: {
+    fr: "\n\n-----\n\n",
+    en: "\n\n-----\n\n",
+  },
+  selectedText: {
+    fr: "*Texte sélectionné*",
+    en: "*Selected text*",
+  },
+};
