ffab1d23e8c1b3702cdd1d36eaf99baaab934fe7
Dominique Climenti <dominique.climenti@kyos.ch>
2023-12-27 18:29:01 +0100

    Refactoring: exported input handling in commands.js and separated logic and commands functions

---
+++ scripts/commands.js
@@ -0,0 +1,347 @@
+async function executeCommand(message) {
+  //
+  // Read webpage command
+  //
+  if (message.startsWith("http://") || message.startsWith("https://")) {
+    getURL(message).then((response) => {
+      enableChat();
+    });
+  } else if (
+    message.endsWith("+i") ||
+    message.startsWith("Cherche ") ||
+    message.startsWith("cherche ") ||
+    message.startsWith("Recherche ") ||
+    message.startsWith("recherche ") || // french
+    message.startsWith("Search ") ||
+    message.startsWith("search ") ||
+    message.startsWith("Find ") ||
+    message.startsWith("find ") // english
+  ) {
+    searchTheWeb(message).then((response) => {
+      enableChat();
+    });
+  } else if (message.startsWith(":readpagecontent")) {
+    readPageContent().then((response) => {
+      enableChat();
+    });
+  } else if (message == ":hist") {
+    hist();
+  } else if (message == ":deleteConfig") {
+    deleteConfig();
+  } else if (message == ":deleteSuggestions") {
+    deleteSuggestions();
+  } else if (message == ":config") {
+    printConfig();
+  } else if (message.startsWith(":set ")) {
+    setValue(message);
+  } else if (message.startsWith(":save ")) {
+    save(message);
+  } else if (message.startsWith(":load ")) {
+    load(message);
+  } else if (message.startsWith(":delete ")) {
+    deleteConv(message);
+  } else if (message == ":list") {
+    listConversations();
+  } else if (message == ":pop") {
+    pop();
+  } else if (message.startsWith(":push ")) {
+    push(message);
+  } else if (message == ":clean") {
+    clean();
+  } else if (message == ":clear") {
+    clear();
+  } else if (message.startsWith(":")) {
+    helpCmd();
+  } else if (message == "Help" || message == "help" || message == "?") {
+    help();
+  } else {
+    notACommand(message);
+  }
+}
+
+async function getURL(message) {
+  text = await getWebpage(message);
+
+  const listMessageBody = document.querySelectorAll(
+    ".chatgeppetto-message-body"
+  );
+  const messageBody = listMessageBody.item(listMessageBody.length - 1);
+  messageBody.innerHTML = "";
+  history.push({
+    role: "system",
+    content:
+      getText("readText") +
+      getText("longSeparator") +
+      text +
+      getText("longSeparator"),
+  });
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+  enableChat();
+}
+
+async function searchTheWeb(message) {
+  removeLastMessage();
+  if (message.endsWith("+i")) {
+    text = message.replace("+i", "");
+  } else {
+    // remove the first word
+    word = message.split(" ").slice(0).join(" ");
+    console.log("word: " + word);
+    text = message.replace(word, "");
+  }
+  history.push({
+    role: "user",
+    content: text,
+  });
+  addChatMessage(you, markdownToHtml(text));
+  addChatMessage(assistant, getText("searching"));
+  let searchQuery = await getSearchQuery(history).then((response) => {
+    return response;
+  });
+  searchQuery = searchQuery.replace('"', "");
+  console.log("Searching the web for: " + searchQuery);
+  var searchUrl = searchEngine + "?q=";
+  console.log(searchUrl);
+  urlsearch = searchUrl + encodeURIComponent(searchQuery);
+  let searchResults = await getSearchResults(urlsearch);
+  getResponse(history).then((response) => {
+    enableChat();
+  });
+  return;
+}
+
+async function readPageContent(message) {
+  text = message.replace(":readpagecontent ", "");
+
+  const listMessageBody = document.querySelectorAll(
+    ".chatgeppetto-message-body"
+  );
+  const messageBody = listMessageBody.item(listMessageBody.length - 1);
+  messageBody.innerHTML = "";
+  history.push({
+    role: "system",
+    content:
+      getText("readText") +
+      getText("longSeparator") +
+      text +
+      getText("longSeparator"),
+  });
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+  addChatMessage(assistant, getText("ok"));
+  history.push({ role: "assistant", content: getText("ok") });
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+  enableChat();
+  return;
+}
+
+async function hist() {
+  removeLastHeader();
+  console.log(history);
+  enableChat();
+  return;
+}
+
+async function deleteConfig() {
+  removeLastHeader();
+  await deleteConfigFromLocalStorage();
+  enableChat();
+  return;
+}
+
+async function deleteSuggestions() {
+  removeLastHeader();
+  deleteInputHistory();
+  enableChat();
+  return;
+}
+
+async function printConfig() {
+  let config = getCurrentConfig();
+  removeLastHeader();
+  console.log(config);
+  enableChat();
+  return;
+}
+
+async function setValue(message) {
+  removeLastHeader();
+  setConfig(message);
+  enableChat();
+  return;
+}
+
+async function save(message) {
+  // remove :save from message
+  message = message.replace(":save ", "");
+  //check if message is a valid string to use as a name for the saved config
+  let exists = await conversationExists(message);
+  if (exists) {
+    addChatMessage(assistant, getText("alreadyExists"));
+    enableChat();
+    return;
+  }
+  if (message.match(/^[a-zA-Z0-9]+$/)) {
+    removeLastHeader();
+    saveConversation(message, history);
+    await addChatMessage(assistant, getText("ok"));
+  } else {
+    addChatMessage(assistant, getText("invalidName"));
+  }
+  enableChat();
+  return;
+}
+
+async function load(message) {
+  message = message.replace(":load ", "");
+  let exists = await conversationExists(message);
+  if (exists) {
+    removeLastHeader();
+    history = await loadConversation(message);
+    await addChatMessage(assistant, getText("ok"));
+    browser.storage.local.set({ hist: JSON.stringify(history) });
+    location.replace(location.href);
+  } else {
+    addChatMessage(assistant, getText("invalidName"));
+  }
+  enableChat();
+}
+
+async function deleteConv(message) {
+  message = message.replace(":delete ", "");
+  let exists = await conversationExists(message);
+  if (exists) {
+    removeLastHeader();
+    await deleteConversation(message);
+    await addChatMessage(assistant, getText("ok"));
+  } else {
+    addChatMessage(assistant, getText("invalidName"));
+  }
+  enableChat();
+}
+
+async function listConversations() {
+  removeLastHeader();
+  let list = await listSavedConversations();
+  console.log(list);
+  let convlist = getText("savedConversations") + "\n\n";
+  for (let i = 0; i < list.length; i++) {
+    convlist += i + 1 + ". " + list[i].key + "\n";
+  }
+  await addChatMessage(assistant, markdownToHtml(convlist));
+  enableChat();
+  return;
+}
+
+async function pop() {
+  removeLastMessage();
+  history.pop();
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+  location.replace(location.href);
+  enableChat();
+  focusInput();
+  return;
+}
+
+async function push(message) {
+  removeLastMessage();
+  message = message.replace(":push ", "");
+  let role = message.split(" ")[0];
+  let content = message.replace(role + " ", "");
+  if (
+    role == "user" ||
+    role == "assistant" ||
+    role == "system" ||
+    role == you ||
+    role == assistant
+  ) {
+    if (role == you) {
+      role = "user";
+      name = you;
+    } else if (role == assistant) {
+      role = "assistant";
+      name = assistant;
+    } else if (role == "user") {
+      name = you;
+    } else if (role == "assistant") {
+      name = assistant;
+    } else if (role == "system") {
+      history.push({
+        role: role,
+        content: content,
+      });
+      browser.storage.local.set({ hist: JSON.stringify(history) });
+      enableChat();
+      return;
+    }
+    addChatMessage(name, markdownToHtml(content));
+    history.push({
+      role: role,
+      content: content,
+    });
+    browser.storage.local.set({ hist: JSON.stringify(history) });
+    enableChat();
+    return;
+  } else {
+    addChatMessage(assistant, getText("invalidRole"));
+    enableChat();
+    return;
+  }
+}
+
+async function clean() {
+  removeLastHeader();
+  history = await cleanHistory(history);
+  browser.storage.local.set({ inputHistory: JSON.stringify(history) });
+  enableChat();
+  addChatMessage(assistant, getText("cleanhistory"));
+  return;
+}
+
+async function clear() {
+  history = [];
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+  enableChat();
+  clearChat();
+  history.push({
+    role: "system",
+    content: getText("systemPrompt"),
+  });
+  history.push({
+    role: "assistant",
+    content: getText("greeting"),
+  });
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+  addChatMessage(assistant, markdownToHtml(getText("greeting")));
+  enableChat();
+  return;
+  //
+  // unknown command
+  //
+}
+
+async function helpCmd() {
+  msg = getText("helpcmd");
+  addChatMessage(assistant, markdownToHtml(msg));
+  enableChat();
+  return;
+  //
+  // Help command
+  //
+}
+
+async function help() {
+  let msg = getText("help");
+  addChatMessage(assistant, markdownToHtml(msg));
+  enableChat();
+  return;
+}
+
+async function notACommand() {
+  let suggestionBox = document.getElementById("suggestionBox");
+  suggestionBox.style.display = "none";
+  await getResponse(history).then((response) => {
+    enableChat();
+  });
+  content;
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+}
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,9 @@
+//
+// UI Management
+//
+//const sendBtn = document.getElementById("chatgeppetto-send");
+//const sendInput = document.getElementById("chatgeppetto-input");
+
 function enableChat() {
   sendBtn.disabled = false;
   sendInput.disabled = false;
@@ -9,10 +15,6 @@ function disableChat() {
   sendInput.disabled = true;
 }
 
-//
-// Function to turn the widget on or off
-// @return void
-//
 function toggleGeppetto() {
   chatVisible = !chatVisible;
   chatWidget.classList.toggle("visible", chatVisible);
@@ -23,44 +25,24 @@ function toggleGeppetto() {
   chatInput.focus();
 }
 
-//
-// Callback functions to read history from the local storage
-// @param item: the item to read
-// @return void
-//
 function onGotHist(item) {
   history = JSON.parse(item.hist);
-  var name = "";
-  for (var i = 0; i < history.length; i++) {
-    if (history[i].role == "user") {
-      name = you;
-    } else if (history[i].role == "assistant") {
-      name = assistant;
-    } else {
-      continue;
+  for (const entry of history) {
+    if (entry.role === "user" || entry.role === "assistant") {
+      const name = entry.role === "user" ? you : assistant;
+      const converter = new showdown.Converter();
+      converter.setFlavor("github");
+      const html = converter.makeHtml(entry.content);
+      addChatMessage(name, html);
+      hljs.highlightAll();
     }
-    var converter = new showdown.Converter();
-    converter.setFlavor("github");
-    var html = converter.makeHtml(history[i].content);
-    addChatMessage(name, html);
-    hljs.highlightAll();
   }
 }
 
-//
-// Callback functions to handle errors when reading history from he local storage
-// @param error: the error to handle
-// @return void
-//
 function onErrorHist(error) {
   history = [];
 }
 
-//
-// Callback functions to read the visibility of the widget from the local storage
-// @param item: the item to read
-// @return void
-//
 function onGotShow(item) {
   chatVisible = item.visible;
   chatWidget.classList.toggle("visible", chatVisible);
@@ -69,36 +51,29 @@ function onGotShow(item) {
   show = item.visible;
 }
 
-//
-// Callback functions to handle errors when reading the visibility of the widget from the local storage
-// @param error: the error to handle
-// @return void
-//
 function onErrorShow(error) {
   show = false;
 }
 
-//
-// Function to handle keypress events
-// @param e: the event to handle
-// @return void
-//
 function KeyPress(e) {
-  var evtobj = window.event ? event : e;
-  if (evtobj.keyCode == 89 && evtobj.ctrlKey) {
+  const evtobj = window.event ? event : e;
+  if (evtobj.keyCode === 89 && evtobj.ctrlKey) {
     toggleGeppetto();
     if (chatVisible) {
       chatInput.focus();
     }
-  } else if (evtobj.keyCode == 69 && evtobj.ctrlKey) {
+  } else if (evtobj.keyCode === 69 && evtobj.ctrlKey) {
     focusInput();
   }
 }
 
+//
+// Text and Config Functions
+//
 function markdownToHtml(text) {
-  var converter = new showdown.Converter();
+  const converter = new showdown.Converter();
   converter.setFlavor("github");
-  var html = converter.makeHtml(text);
+  const html = converter.makeHtml(text);
   return html;
 }
 
@@ -107,27 +82,29 @@ function onlyUnique(value, index, array) {
 }
 
 function getText(textName) {
-  // Check if the text name exists
   if (statements[textName]) {
-    // Check if the specified language exists, fallback to English if not found
     const selectedLanguage = statements[textName][language] ? language : "en";
-    return statements[textName][selectedLanguage];
+    return statements[textName][selectedLanguage] || "Text not found";
   } else {
     return "Text not found";
   }
 }
 
-// Calling code
+//
+// Config Management
+//
 async function getConfigAndApply() {
   try {
     const config = await readConfigFromLocalStorage();
     applyConfig(config);
   } catch (error) {
-    // Handle errors here
     console.error("Error:", error);
   }
 }
 
+//
+// History Functions
+//
 function cleanHistory(history) {
   return history.filter(
     (message) => message.role === "user" || message.role === "assistant"
@@ -135,29 +112,27 @@ function cleanHistory(history) {
 }
 
 function focusInput() {
-  console.log("focus");
-  let inputField = document.getElementById("chatgeppetto-input");
+  const inputField = document.getElementById("chatgeppetto-input");
   setTimeout(() => {
     inputField.focus();
   }, 100);
 }
 
+//
+// Message Removal Functions
+//
+function removeLastElement(selector) {
+  const list = document.querySelectorAll(selector);
+  const lastElement = list.item(list.length - 1);
+  lastElement && lastElement.remove();
+}
+
 function removeLastHeader() {
-  //remove last div whith class chatgeppetto-message-header
-  const listMessageHeader = document.querySelectorAll(
-    ".chatgeppetto-message-header"
-  );
-  const messageHeader = listMessageHeader.item(listMessageHeader.length - 1);
-  messageHeader.remove();
+  removeLastElement(".chatgeppetto-message-header");
 }
 
 function removeLastBody() {
-  //remove last div whith class chatgeppetto-message-body
-  const listMessageBody = document.querySelectorAll(
-    ".chatgeppetto-message-body"
-  );
-  const messageBody = listMessageBody.item(listMessageBody.length - 1);
-  messageBody.remove();
+  removeLastElement(".chatgeppetto-message-body");
 }
 
 function removeLastMessage() {
@@ -166,16 +141,9 @@ function removeLastMessage() {
 }
 
 function clearChat() {
-  const listMessageBody = document.querySelectorAll(
-    ".chatgeppetto-message-body"
-  );
-  const listMessageHeader = document.querySelectorAll(
-    ".chatgeppetto-message-header"
-  );
-  for (let i = 0; i < listMessageBody.length; i++) {
-    listMessageBody.item(i).remove();
-  }
-  for (let i = 0; i < listMessageHeader.length; i++) {
-    listMessageHeader.item(i).remove();
-  }
+  document
+    .querySelectorAll(
+      ".chatgeppetto-message-body, .chatgeppetto-message-header"
+    )
+    .forEach((element) => element.remove());
 }
