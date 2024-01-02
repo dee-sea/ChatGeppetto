aa9cdf74d365d6f255b9bae79a6ded91086bb12b
Dominique Climenti <dom@thele.me>
2024-01-02 20:18:50 +0100

    Refactoring: Refactored response.js

--- scripts/response.js
+++ scripts/response.js
@@ -1,255 +1,279 @@
-//
 // Function to get the stream response from the API endpoint
-//
 async function getResponse(history, continuation = false, realHistory = []) {
   disableChat();
-  var source;
+
   if (!readConfigFromLocalStorage()) {
-    addChatMessage(
-      "Geppetto",
-      "Please set the API endpoint in the options with: `:set api <endpoint URL>`.\n\n For example: `:set api https://api.openai.com/v1/chat/completions` to use OpenAI.",
-      true,
-    );
+    showConfigError();
     return;
-  }
-  if (GEPPETTO_API_ENDPOINT == "") {
-    addChatMessage(
-      "Geppetto",
-      "Please set the API endpoint in the options with: `:set api <endpoint URL>`.\n\n For example: `:set api https://api.openai.com/v1/chat/completions` to use OpenAI.",
-      true,
-    );
-    return;
-  } else if (GEPPETTO_API_ENDPOINT.startsWith("https://api.openai.com/")) {
-    source = await fetch(GEPPETTO_API_ENDPOINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${GEPPETTO_API_KEY}`,
-      },
-      body: JSON.stringify({
-        messages: history,
-        model: "gpt-3.5-turbo",
-        stream: false,
-        temperature: temperature,
-        max_tokens: maxTokens,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        return data.choices[0].message.content;
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  } else {
-    if (continuation) {
-      let endpoint = GEPPETTO_API_ENDPOINT.replace("chat/", "");
-      console.log(endpoint);
-      let text = history[history.length - 1].content;
-      console.log("XXX" + text + "XXX");
-      source = new SSE(endpoint, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${GEPPETTO_API_KEY}`,
-        },
-        payload: JSON.stringify({
-          max_tokens: maxTokens,
-          prompt: text,
-          temperature: temperature,
-          stream: true,
-        }),
-      });
-    } else {
-      let endpoint = GEPPETTO_API_ENDPOINT;
-      source = new SSE(endpoint, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${GEPPETTO_API_KEY}`,
-        },
-        payload: JSON.stringify({
-          messages: history,
-          mode: "chat-instruct",
-          instruction_template: template,
-          character: character,
-          stream: true,
-          temperature: temperature,
-          max_tokens: maxTokens,
-        }),
-      });
-    }
   }
 
+  const source = await getSource(history, continuation);
+
   if (GEPPETTO_API_ENDPOINT.startsWith("https://api.openai.com/")) {
+    handleOpenAIResponse(source, history);
+  } else {
+    handleSSEResponse(source, history, continuation, realHistory);
+  }
+
+  enableChat();
+}
+
+// Function to handle OpenAI API response
+async function handleOpenAIResponse(source, history) {
+  try {
+    const listMessageBody = document.querySelectorAll(
+      ".chatgeppetto-message-body",
+    );
+    const messageBody = listMessageBody.item(listMessageBody.length - 1);
+
+    const content = await getOpenAIContent(source);
+
+    const html = markdownToHtml(content);
+    messageBody.innerHTML = html;
+
+    hljs.highlightAll();
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+
+    history.push({ role: "assistant", content: content });
+
+    browser.storage.local.set({ hist: JSON.stringify(history) });
+    sendInput.disabled = false;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// Function to handle SSE response
+function handleSSEResponse(source, history, continuation, realHistory) {
+  // Function to handle the SSE error response
+  source.addEventListener("error", function (e) {
+    console.error("SSE Error:", e);
+  });
+
+  // Function to handle the SSE response
+  source.addEventListener("message", async function (e) {
     try {
+      var payload = JSON.parse(e.data);
+      if (payload.choices[0].finish_reason != "stop") {
+        handleNonStopFinishReason(payload, continuation, history);
+      } else {
+        handleStopFinishReason(payload, history, continuation, realHistory);
+      }
+    } catch (e) {
+      browser.storage.local.set({ hist: JSON.stringify(history) });
       const listMessageBody = document.querySelectorAll(
         ".chatgeppetto-message-body",
       );
       const messageBody = listMessageBody.item(listMessageBody.length - 1);
-      let html = markdownToHtml(source);
-      messageBody.innerHTML = html;
-      hljs.highlightAll();
-      chatMessages.scrollTop = chatMessages.scrollHeight;
-      history.push({ role: "assistant", content: source });
-      browser.storage.local.set({ hist: JSON.stringify(history) });
-      sendInput.disabled = false;
-    } catch (e) {
-      console.error(e);
+      const contButton = document.createElement("button");
+      contButton.classList.add("chatgeppetto-cont");
+      contButton.innerHTML = "...";
+      contButton.setAttribute("title", "Continue");
     }
-  } else {
-    //
-    // Function to handle the SSE error response
-    //
-    source.addEventListener("error", function (e) {
-      console.error("SSE Error:", e);
-    });
+  });
+}
 
-    //
-    // Function to handle the SSE response
-    //
-    source.addEventListener("message", async function (e) {
-      // Assuming we receive JSON-encoded data payloads:
-      try {
-        var payload = JSON.parse(e.data);
-      } catch (e) {
-        browser.storage.local.set({ hist: JSON.stringify(history) });
-        const listMessageBody = document.querySelectorAll(
-          ".chatgeppetto-message-body",
-        );
-        const messageBody = listMessageBody.item(listMessageBody.length - 1);
-        const contButton = document.createElement("button");
-        contButton.classList.add("chatgeppetto-cont");
-        contButton.innerHTML = "...";
-        contButton.setAttribute("title", "Continue");
-      }
-      console.log(e.data);
-      if (payload.choices[0].finish_reason != "stop") {
-        if (payload.choices[0].finish_reason == "length") {
-          console.log("length reason");
-          // get the last div with class chatgeppetto-message-body
-          const listMessageBody = document.querySelectorAll(
-            ".chatgeppetto-message-body",
-          );
-          const messageBody = listMessageBody.item(listMessageBody.length - 1);
-          const contButton = document.createElement("button");
-          contButton.classList.add("chatgeppetto-cont");
-          contButton.innerHTML = "...";
-          contButton.setAttribute("title", "Continue");
-          messageBody.appendChild(contButton);
-        } else {
-          var chatbotResponseElement;
-          if (continuation) {
-            chatbotResponseElement = payload.choices[0].text;
-          } else {
-            chatbotResponseElement = payload.choices[0].delta.content;
-          }
-          appendChatElement(chatbotResponseElement);
-          chatMessages.scrollTop = chatMessages.scrollHeight;
-        }
-      } else {
-        const listMessageBody = document.querySelectorAll(
-          ".chatgeppetto-message-body",
-        );
-        const messageBody = listMessageBody.item(listMessageBody.length - 1);
-        let contextLength = estimateContextLength(history);
-        // add the last message to the history
-        let lastUserMessage = history
-          .filter((message) => message.role === "user")
-          .pop().content;
-        if (continuation) {
-          answer = lastUserMessage.replace(getText("continue"), "") + answer;
-        }
-        history.push({ role: "assistant", content: answer });
-        if (contextLength > cLength) {
-          addChatMessage(
-            "ChatGeppetto",
-            "I'm sorry, I can't remember that far back.",
-            true,
-          );
-          // if conversation contains elements with the system role
-          await clean();
-          const list = document.querySelectorAll(".chatgeppetto-message-body");
-          const last = list.item(list.length - 2);
-          const lastText = last.textContent;
-          last.innerHTML = markdownToHtml(
-            lastText + "\n\nContext length: " + contextLength,
-          );
-        }
-        contextLength = estimateContextLength(history);
-        if (contextLength > cLength) {
-          addChatMessage(
-            "ChatGeppetto",
-            markdownToHtml(
-              "Context length too long, generating conversation summary, please wait.",
-            ),
-            true,
-          );
-          // split the history in 2 parts. one called lastHistory and the other called oldHistory
-          // last history will contain the last 6 messages and oldHistory will contain the rest
-          let lastHistory = [];
-          let oldHistory = [];
-          for (let i = 0; i < history.length; i++) {
-            if (i < history.length - keep * 2) {
-              oldHistory.push(history[i]);
-            } else {
-              lastHistory.push(history[i]);
-            }
-          }
-          summary = await summarizeConversation(oldHistory);
-          history = [];
-          history.push({
-            role: "assistant",
-            content: summary,
-          });
-          browser.storage.local.set({ hist: JSON.stringify(history) });
-          for (message of lastHistory) {
-            history.push(message);
-          }
-          browser.storage.local.set({ hist: JSON.stringify(history) });
-          removeLastMessage();
-        }
-        contextLength = estimateContextLength(history);
-        if (contextLength > cLength) {
-          if (cLength < 32000) {
-            cLength = cLength * 2;
-            addChatMessage(
-              "ChatGeppetto",
-              markdownToHtml("extentding context length to:" + cLength + "."),
-              true,
-            );
-          } else {
-            addChatMessage(
-              "ChatGeppetto",
-              markdownToHtml("context length too long:" + cLength + "."),
-              true,
-            );
-          }
-        }
-        if (continuation) {
-          history = realHistory;
-          history.pop();
-          history.pop();
-          history.push({ role: "assistant", content: answer });
-        }
-        let html = markdownToHtml(answer);
-        messageBody.innerHTML = html;
-        // get the last div with class chatgeppetto-message-body
-        const listMessageBody2 = document.querySelectorAll(
-          ".chatgeppetto-message-body",
-        );
-        hljs.highlightAll();
-        chatMessages.scrollTop = chatMessages.scrollHeight;
-        browser.storage.local.set({ hist: JSON.stringify(history) });
-        sendInput.disabled = false;
-        answer = "";
-      }
-    });
+// Function to handle non-stop finish reason
+function handleNonStopFinishReason(payload, continuation, history) {
+  if (payload.choices[0].finish_reason == "length") {
+    handleLengthFinishReason(payload, history);
+  } else {
+    handleOtherFinishReason(payload, continuation);
   }
-  // if the role of the last message is "user" then add the answer to the history
-  if (
-    history[history.length - 1].role === "user" ||
-    history[history.length - 1].role === "system"
-  ) {
+}
+
+// Function to handle stop finish reason
+function handleStopFinishReason(payload, history, continuation, realHistory) {
+  const listMessageBody = document.querySelectorAll(
+    ".chatgeppetto-message-body",
+  );
+  const messageBody = listMessageBody.item(listMessageBody.length - 1);
+
+  let contextLength = estimateContextLength(history);
+
+  // Add the last message to the history
+  let lastUserMessage = history
+    .filter((message) => message.role === "user")
+    .pop().content;
+
+  if (continuation) {
+    answer = lastUserMessage.replace(getText("continue"), "") + answer;
+  }
+
+  history.push({ role: "assistant", content: answer });
+
+  handleContextLength(contextLength, history, continuation, realHistory);
+
+  let html = markdownToHtml(answer);
+  messageBody.innerHTML = html;
+
+  const listMessageBody2 = document.querySelectorAll(
+    ".chatgeppetto-message-body",
+  );
+  hljs.highlightAll();
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+  sendInput.disabled = false;
+  answer = "";
+}
+
+// Function to handle SSE error
+function handleError(e, history) {
+  browser.storage.local.set({ hist: JSON.stringify(history) });
+
+  const listMessageBody = document.querySelectorAll(
+    ".chatgeppetto-message-body",
+  );
+  const messageBody = listMessageBody.item(listMessageBody.length - 1);
+
+  const contButton = document.createElement("button");
+  contButton.classList.add("chatgeppetto-cont");
+  contButton.innerHTML = "...";
+  contButton.setAttribute("title", "Continue");
+}
+
+// Function to handle finish reason "length"
+function handleLengthFinishReason(payload, history) {
+  const listMessageBody = document.querySelectorAll(
+    ".chatgeppetto-message-body",
+  );
+  const messageBody = listMessageBody.item(listMessageBody.length - 1);
+
+  const contButton = document.createElement("button");
+  contButton.classList.add("chatgeppetto-cont");
+  contButton.innerHTML = "...";
+  contButton.setAttribute("title", "Continue");
+  messageBody.appendChild(contButton);
+}
+
+// Function to handle finish reasons other than "stop" and "length"
+function handleOtherFinishReason(payload, continuation) {
+  const listMessageBody = document.querySelectorAll(
+    ".chatgeppetto-message-body",
+  );
+  const messageBody = listMessageBody.item(listMessageBody.length - 1);
+
+  let chatbotResponseElement;
+
+  if (continuation) {
+    chatbotResponseElement = payload.choices[0].text;
+  } else {
+    chatbotResponseElement = payload.choices[0].delta.content;
+  }
+
+  appendChatElement(chatbotResponseElement);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+// Function to handle context length
+function handleContextLength(
+  contextLength,
+  history,
+  continuation,
+  realHistory,
+) {
+  if (contextLength > cLength) {
+    handleContextLengthExceed(contextLength, history);
+  }
+
+  if (continuation) {
+    history = realHistory;
+    history.pop();
+    history.pop();
     history.push({ role: "assistant", content: answer });
   }
-  enableChat();
+}
+
+// Function to handle context length exceed
+async function handleContextLengthExceed(contextLength, history) {
+  addChatMessage(
+    "ChatGeppetto",
+    "I'm sorry, I can't remember that far back.",
+    true,
+  );
+
+  await clean();
+
+  const list = document.querySelectorAll(".chatgeppetto-message-body");
+  const last = list.item(list.length - 2);
+  const lastText = last.textContent;
+
+  last.innerHTML = markdownToHtml(
+    lastText + "\n\nContext length: " + contextLength,
+  );
+}
+
+// Function to get OpenAI content
+async function getOpenAIContent(source) {
+  const response = await fetch(GEPPETTO_API_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${GEPPETTO_API_KEY}`,
+    },
+    body: JSON.stringify({
+      messages: history,
+      model: "gpt-3.5-turbo",
+      stream: false,
+      temperature: temperature,
+      max_tokens: maxTokens,
+    }),
+  });
+
+  const data = await response.json();
+  return data.choices[0].message.content;
+}
+
+// Function to get SSE source
+function getSource(history, continuation) {
+  let source;
+
+  if (continuation) {
+    source = getContinuationSSE(history);
+  } else {
+    source = getNewSSE(history);
+  }
+
+  return source;
+}
+
+// Function to get new SSE
+function getNewSSE(history) {
+  let endpoint = GEPPETTO_API_ENDPOINT;
+
+  return new SSE(endpoint, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${GEPPETTO_API_KEY}`,
+    },
+    payload: JSON.stringify({
+      messages: history,
+      mode: "chat-instruct",
+      instruction_template: template,
+      character: character,
+      stream: true,
+      temperature: temperature,
+      max_tokens: maxTokens,
+    }),
+  });
+}
+
+// Function to get continuation SSE
+function getContinuationSSE(history) {
+  let endpoint = GEPPETTO_API_ENDPOINT.replace("chat/", "");
+  let text = history[history.length - 1].content;
+
+  return new SSE(endpoint, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${GEPPETTO_API_KEY}`,
+    },
+    payload: JSON.stringify({
+      max_tokens: maxTokens,
+      prompt: text,
+      temperature: temperature,
+      stream: true,
+    }),
+  });
 }
